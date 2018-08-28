8b274eb6441fce916a868c0e7fd5a118b7106fa6
mostar <mominahmad93@gmail.com>
2018-08-28 01:46:46 -0500

    shitty but (almost) works

---
+++ bamazonMGMT.js
@@ -0,0 +1,121 @@
+var inquire = require("inquirer");
+var mysql = require("mysql");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 8889,
+
+    user: "root",
+
+    password: "root",
+    database: "bamazon_db",
+
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+});
+
+function mgmtChoices() {
+    inquire.prompt([
+        {
+            type: "list",
+            message: "what would you like to do?",
+            choices: ["view inventory", "view low inventory", "add to inventory", "add product"],
+            name: "mgmtChoices"
+        }
+    ]).then(function (res) {
+        switch (res.mgmtChoices) {
+            case "view inventory":
+                showInventory();
+                break;
+            case "view low inventory":
+                showLowInventory();
+                break;
+            case "add to inventory":
+            connection.query("SELECT * FROM products", function (err, res) {
+                console.log("==========================================================\n");
+                console.table(res);
+                console.log("\n=========================================================")
+            })
+                inquire.prompt([
+                    {
+                        type: "input",
+                        message: "what item would you like to restock? (choose ID #)",
+                        name: "productID"
+                    },
+                    {
+                        type: "input",
+                        message: "how much would you like to restock?",
+                        name: "restockCount"
+                    }
+                ]).then (function(res){
+                    console.log("working");
+                    console.log(res.productID);
+                    console.log(res.restockCount);
+                    addToInventory(res.restockCount, res.productID);
+                })
+                break;
+                case "add product":
+                inquire.prompt ([
+                {
+                    type: "input",
+                    message: "what is the name of the product?",
+                    name: "productName"
+                },
+                {
+                    type: "input",
+                    message: "what department?",
+                    name: "productDept"
+                },
+                {
+                    type: "input",
+                    message: "what price?",
+                    name: "productPrice"
+                },
+                {
+                    type: "input",
+                    message: "how much would you like to stock?",
+                    name: "productStock"
+                }
+            ]).then (function(res){
+                connection.query("INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)", [res.productName, res.productDept, res.productPrice, res.productStock], function(){
+                    showInventory();
+                })
+            })
+        }
+    })
+}
+
+
+function showInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log("==========================================================\n");
+        console.table(res);
+        console.log("\n=========================================================")
+        mgmtChoices();
+    })
+}
+
+function showLowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 6", function (err, res) {
+        console.table(res);
+        mgmtChoices();
+    })
+
+}
+
+function addToInventory(quantity, id) {
+    connection.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [quantity, id], function(){
+        showInventory();
+        mgmtChoices();
+    })
+}
+
+mgmtChoices();
+
+
